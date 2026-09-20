9d0821efc09e5fbcbe25ad5cee3509dad341d7c8
magicsea <magicsea_wy@163.com>
2026-09-20 20:42:01 +0800

    ndk fix

--- buildSrc/src/main/kotlin/AppConfig.kt
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -2,7 +2,7 @@ object AppConfig {
     const val compileSdkVersion = 37
     const val targetSdkVersion = compileSdkVersion
     const val minSdkVersion = 24
-    const val ndkVersion = "28.0.13004108"
+    const val ndkVersion = "27.2.12479018"  // HACK(B): use local NDK 27.2 instead of 28
 
     const val versionCode = 41
     const val versionName = "2.0.1"
